5eb6afbf23de1e71e88e42c461823d9a80d2aa30
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-14 21:16:28 -0400

    finished add step

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -148,13 +148,18 @@ async function findSteps(scheme_id) { // EXERCISE C
 }
 
 function add(scheme) { // EXERCISE D
-
+  return db('schemes').insert(scheme)
+    .then(([scheme_id]) =>{
+      return db('schemes').where('scheme_id', scheme_id).first()
+    })
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+async function addStep(scheme_id, step) { // EXERCISE E
+  await db('steps').insert({...step, scheme_id})
+  return findSteps(scheme_id)
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
