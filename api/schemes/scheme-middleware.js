698679d0a880bee98b3344a8e0ded39b64910d0f
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-14 20:33:15 -0400

    finished validatestep

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -33,9 +33,9 @@ const checkSchemeId = async (req, res, next) => {
 */
 const validateScheme = (req, res, next) => {
   const {scheme_name} = req.body;
-  if(!scheme_name ||
+  if(scheme_name === undefined||
      typeof scheme_name !== 'string' ||
-      scheme_name === undefined) {
+     !scheme_name.trim() ) {
     next({status: 400, message: 'invalid scheme_name' });
   } else {
     next();
@@ -53,12 +53,15 @@ const validateScheme = (req, res, next) => {
 */
 const validateStep = (req, res, next) => {
   const {instructions, step_number} = req.body;
-  if (!instructions || typeof instructions !== 'string') {
-    res.status(400).json({message:'invalid instructions'});
-  } else if (step_number < 1) {
-    res.status(400).json({message:'invalid step'});
+  if (instructions === undefined ||
+     typeof instructions !== 'string' ||
+     !instructions.trim() ||
+     typeof step_number !== 'number' ||
+     step_number < 1) {
+      const error = {status: 400, message: 'invalid step'};
+      next(error);
   } else {
-    next();
+      next();
   }
 }
 
